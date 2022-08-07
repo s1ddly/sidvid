7a612e292bdd98066befa3b586f65a5608dc8ae7
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-08-07 20:59:14 +1000

    Add gitignore, discord.py import and improve logging

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+echo "Starting server!"
+echo "Setting up Virtual Environment"
 pip install virtualenv
 python3 -m virtualenv sidvidpy
 source sidvidpy/bin/activate
+echo "Installing the required libraries in the venv"
 pip install pytube
+pip install discord.py
+echo "Executing python"
 python3 sidvidserver.py
+echo "Shutting down venv "
 deactivate
+echo "Successfully shut down the server!"
--- sidvidserver.py
+++ sidvidserver.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=x7X9w_GIm1s')
-print(yt.title)
-print("Filesize: " + str(yt.streams.filter(progressive=True).order_by('resolution').desc().first().filesize))
-yt.streams.filter(progressive=True).order_by('resolution').desc().first().download(output_path="./output")
+import os
+import discord
+
+
+
+
+def yt_test():
+    yt = YouTube('https://www.youtube.com/watch?v=x7X9w_GIm1s')
+    print(yt.title)
+    print("Filesize: " + str(yt.streams.filter(progressive=True).order_by('resolution').desc().first().filesize))
+    yt.streams.filter(progressive=True).order_by('resolution').desc().first().download(output_path="./output")
