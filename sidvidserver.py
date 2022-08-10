592847ccbe09f42ab944e38df80d70f08178441d
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-08-10 20:43:02 +1000

    updated code

--- sidvidserver.py
+++ sidvidserver.py
@@ -2,12 +2,37 @@
 from pytube import YouTube
 import os
 import discord
+from discord import File
+import time
+from datetime import datetime
 
+token = open("token","r").read()
 
+client = discord.Client()
 
+def yt_dl(inlink, fname):
+    yt = YouTube(inlink)
+    print(str(datetime.now()) + " " + yt.title)
+    print(str(datetime.now()) + " Filesize: " + str(yt.streams.filter(progressive=True).order_by('resolution').desc().first().filesize))
+    yt.streams.filter(progressive=True).order_by('resolution').desc().first().download(output_path="./output", filename=fname)
+    return("output/" + fname)
+    
+def yt_getsize(inlink):
+    yt = YouTube(inlink)
+    return(yt.streams.filter(progressive=True).order_by('resolution').desc().first().filesize)
 
-def yt_test():
-    yt = YouTube('https://www.youtube.com/watch?v=x7X9w_GIm1s')
-    print(yt.title)
-    print("Filesize: " + str(yt.streams.filter(progressive=True).order_by('resolution').desc().first().filesize))
-    yt.streams.filter(progressive=True).order_by('resolution').desc().first().download(output_path="./output")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        if message.content[0:4] == "-dl ":
+            invid = message.content.split(" ")[1]
+            print(str(datetime.now()) + " " + message.author.name + " Tried to download: " + invid)
+            
+            if yt_getsize(invid) < 8388608:
+                await message.channel.send(message.author.mention, file=File(yt_dl(message.content,str(int(time.time_ns())) + ".mp4")))
+            else:
+                await message.channel.send(message.author.mention + " Failed to download, file is over 8mb(" + str(yt_getsize(invid)) + " bytes) :(")
+    
+client.run(token)
