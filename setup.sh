6d87ee536c20f216284f5e8fd5ad21d5c633683f
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-08-11 22:36:27 +1000

    adding -u flag for live output to file

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ echo "Installing the required libraries in the venv"
 pip install pytube==12.1.0
 pip install discord.py==1.7.3
 echo "Executing python"
-python3 sidvidserver.py
+python3 -u sidvidserver.py
 echo "Shutting down venv "
 deactivate
 echo "Successfully shut down the server!"
