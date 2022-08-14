494f03d222afe96861dbffb287dd194240eb205c
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-08-14 16:04:41 +1000

    Adding in support for >50mb and >100mb videos for boosted servers

--- sidvidserver.py
+++ sidvidserver.py
@@ -28,12 +28,23 @@ async def on_message(message):
     else:
         if message.content[0:4] == "-dl ":
             invid = message.content.split(" ")[1]
-            print(str(datetime.now()) + " " + message.author.name + " Tried to download: " + invid)
+            print(str(datetime.now()) + " " + message.author.name + ", " + message.guild.name + " Tried to download: " + invid)
             try:
-                if yt_getsize(invid) < 8388608:
-                    await message.channel.send(message.author.mention, file=File(yt_dl(message.content,str(int(time.time_ns())) + ".mp4")))
-                else:
-                    await message.channel.send(message.author.mention + " Failed to download, file is over 8mb(" + str(yt_getsize(invid)) + " bytes) :(")
+                if message.guild.premium_tier == 0 or message.guild.premium_tier == 1:
+                    if yt_getsize(invid) < 8388608:
+                        await message.channel.send(message.author.mention, file=File(yt_dl(message.content,str(int(time.time_ns())) + ".mp4")))
+                    else:
+                        await message.channel.send(message.author.mention + " Failed to download, file is over 8mb(" + str(yt_getsize(invid)) + " bytes) :(")
+                elif message.guild.premium_tier == 2:
+                    if yt_getsize(invid) < 52428800:
+                        await message.channel.send(message.author.mention, file=File(yt_dl(message.content,str(int(time.time_ns())) + ".mp4")))
+                    else:
+                        await message.channel.send(message.author.mention + " Failed to download, file is over 50mb(" + str(yt_getsize(invid)) + " bytes) :(")
+                elif message.guild.premium_tier == 3:
+                    if yt_getsize(invid) < 104857600:
+                        await message.channel.send(message.author.mention, file=File(yt_dl(message.content,str(int(time.time_ns())) + ".mp4")))
+                    else:
+                        await message.channel.send(message.author.mention + " Failed to download, file is over 100mb(" + str(yt_getsize(invid)) + " bytes) :(")
             except:
                 await message.channel.send(message.author.mention + " There was an error with the download :o, please check the message and try again")
 client.run(token)
