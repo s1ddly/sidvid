8378adcc32a221fdd572b1d988f958347db6c4fe
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-08-28 22:04:17 +1000

    Introducing bin folder for executables

--- setup.sh
+++ setup.sh
@@ -7,8 +7,9 @@ source sidvidpy/bin/activate
 echo "Installing the required libraries in the venv"
 pip install pytube==12.1.0
 pip install discord.py==1.7.3
+pip install selenium==4.4.3
 echo "Executing python"
-python3 -u sidvidserver.py
+python3 -u bin/sidvidserver.py
 echo "Shutting down venv "
 deactivate
 echo "Successfully shut down the server!"
